6e684f13ac4a36bf5fab6a619458a7e0fd7001d5
ujjawalkumarsingh <singhujjawal9793@gmail.com>
2023-09-05 17:39:28 +0530

    Initial Commit

---
+++ ujjawal card swiftui/ujjawal_card_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ujjawal_card_swiftuiApp.swift
+//  ujjawal card swiftui
+//
+//  Created by Ujjawal Kumar Singh on 05/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ujjawal_card_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
