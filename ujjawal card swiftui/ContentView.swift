560b31b6e33c4d95f36fd14ce928f51f1d7f7191
ujjawalkumarsingh <singhujjawal9793@gmail.com>
2023-09-07 11:20:01 +0530

    completed

--- ujjawal card swiftui/ContentView.swift
+++ ujjawal card swiftui/ContentView.swift
@@ -9,18 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color(.systemGreen)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("ujjawal-image")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200.0, height: 200.0)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                Text("Ujjawal Singh")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(Color.white)
+                Text("iOS Developer")
+                    .font(.system(size:25))
+                    .foregroundColor(Color.white)
+                Divider()
+                InfoView(text: "+91 435643467", imageName: "phone.fill")
+                InfoView(text: "ujjawal19s@gmail.com", imageName: "envelope.fill")
+            }
         }
-        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct InfoView: View {
+    let text : String
+    let imageName : String
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .padding(.horizontal)
+            .foregroundColor(Color.white)
+            .frame(height: 50.0)
+            
+            .overlay(HStack{
+                Image(systemName: imageName)
+                    .foregroundColor(.green)
+                Text(text)
+                    
+            })
     }
 }
